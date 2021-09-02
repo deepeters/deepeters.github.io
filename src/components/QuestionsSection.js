702d443d467d4afd360cea6bfc02ac5ff971c682
Dennis Peters <dennis95peters@gmail.com>
2021-09-02 12:45:49 +0300

    Implement global and custom styles to the FAQ section

--- src/components/QuestionsSection.js
+++ src/components/QuestionsSection.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
+//Styles
+import styled from "styled-components";
+import { StyledAbout } from '../styles';
+
 const QuestionsSection = () => {
     return (
-        <div className="questions">
-            <h2> <span>Questions</span> commonly asked about Dennis</h2>
+        <StyledQuestions>
+            <h2> Any Questions? <span> FAQs </span> </h2>
             <div className="question">
                 <h4> Where did Dennis attend school?</h4>
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4> Where has Dennis worked?</h4>
@@ -17,6 +22,7 @@ const QuestionsSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4> What frameworks is Dennis most familiar with?</h4>
@@ -24,6 +30,7 @@ const QuestionsSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
             <div className="question">
                 <h4> Where does Dennis want to work? </h4>
@@ -31,9 +38,38 @@ const QuestionsSection = () => {
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
                 </div>
+                <div className="faq-line"></div>
             </div>
-        </div>
+        </StyledQuestions>
     )
 }
+
+const StyledQuestions = styled (StyledAbout)`
+    display: block;
+    span {
+        display: block;
+    }
+    h2 {
+        padding-bottom: 2rem;
+        font-weight: lighter;
+    }
+    .faq-line {
+        background: #cccccc;
+        height: 0.2rem;
+        margin: 2rem 0rem;
+        width: 100%;
+    }
+    .question {
+        padding: 3rem 0rem;
+        cursor: pointer;
+    }
+    .answer {
+        padding: 2rem 0rem;
+        p {
+            padding: 1rem 0rem;
+        }
+    }
+`;
+
 
 export default QuestionsSection;
