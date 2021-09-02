f2764909e7ed9ee77015bd19f2b3748693d3357c
Dennis Peters <dennis95peters@gmail.com>
2021-09-02 15:32:20 +0300

    Introduce React Router DOM to assist in navigating the different pages of the project

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React from 'react';
 //Global Style
 import GlobalStyle from './components/GlobalStyle';
 
+//Navigation Bar
+import Nav from './components/Nav'
+
 //Import Pages
 import About from './pages/About';
+import Contact from './pages/Contact';
+import Projects from './pages/Projects';
+
+//Import from Router
+import {Switch , Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <About />
-      <GlobalStyle />
+      < GlobalStyle />
+      < Nav />
+      <Switch>
+        < Route path="/" exact>
+          < About />
+        </Route>
+        <Route path="/projects">
+          < Projects />
+        </Route>
+        <Route path="/contact">
+          < Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
