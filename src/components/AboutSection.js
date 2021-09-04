981dbe72e326d64a91662cdc522044b9bad9fa1b
Dennis Peters <dennis95peters@gmail.com>
2021-09-04 08:59:47 +0300

    Add more animation to page components

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -6,6 +6,7 @@ import {StyledAbout, StyledDescription, StyledImage, StyledHide } from "../style
 
 //Framer Motion
 import { motion } from 'framer-motion';
+import { titleAnim, fade, photoAnim } from '../animation';
 
 const AboutSection = () => {
 
@@ -15,26 +16,31 @@ const AboutSection = () => {
             <StyledDescription>
                 <motion.div className="title">
                     <StyledHide>
-                        <motion.h2>
+                        <motion.h2 variants = { titleAnim }>
                             Hello, I am Dennis
                         </motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2>
+                        <motion.h2 variants = { titleAnim }>
                              a <span> Software Engineer </span> & 
                         </motion.h2>
                     </StyledHide>
                     <StyledHide>
-                        <motion.h2>
+                        <motion.h2 variants = { titleAnim }>
                             an African literature enthuthiast
                         </motion.h2>
                     </StyledHide>
                 </motion.div>
-                <p> Contact me for discussions and collaborations </p>
-                <button>Contact</button>
+                <motion.p variants = { fade }> 
+                    Contact me for discussions and collaborations.
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus dolor commodi neque officia et magnam. Consequatur sequi maxime perferendis aperiam.
+                </motion.p>
+                <motion.button variants = { fade }>
+                    Contact
+                </motion.button>
             </StyledDescription>
             <StyledImage>
-                <img src={ about } alt="Dennis is a portrait" />
+                <motion.img variants = { photoAnim } src={ about } alt="Dennis is a portrait" />
             </StyledImage>
         </StyledAbout>
     )
