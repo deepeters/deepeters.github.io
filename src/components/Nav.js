ae5f6c2dd64fbca0f39fa776a384d34079064b3d
Dennis Peters <dennis95peters@gmail.com>
2021-09-05 12:26:00 +0300

    Animate the navigation bar and ensure it is mobile friendly

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 
 const Nav = () => {
+
+    const {pathname} = useLocation();
+
     return (
         <StyledNav>
             <h1> <Link id="logo" to = "/"> Portfolio </Link> </h1>
             <ul>
                 <li>
-                    <Link to="/"> 1. About </Link>
+                    <Link to="/"> ABOUT </Link>
+                    <Line 
+                        transition = {{ duration: 0.75 }} 
+                        initial = {{ width: '0%' }} 
+                        animate = {{ width: pathname === '/' ? '50%' : '0%' }}
+                    />
                 </li>
                 <li>
-                    <Link to="/projects"> 2. Projects </Link>
+                    <Link to="/projects"> PROJECTS </Link>
+                    <Line 
+                        transition = {{ duration: 0.75 }} 
+                        initial = {{ width: '0%' }} 
+                        animate = {{ width: pathname === '/projects' ? '50%' : '0%' }}
+                    />
                 </li>
                 <li>
-                    <Link to="/contact"> 3. Contact </Link>
+                    <Link to="/contact"> CONTACT </Link>
+                    <Line 
+                        transition = {{ duration: 0.75 }} 
+                        initial = {{ width: '0%' }} 
+                        animate = {{ width: pathname === '/contact' ? '50%' : '0%' }}
+                    />
                 </li>
             </ul>
         </StyledNav>
@@ -68,6 +87,18 @@ const StyledNav = styled.nav `
                 padding: 0;
             }
         }
+    }
+`;
+
+const Line = styled(motion.div)`
+    height: 0.3rem;
+    background: #23d997;
+    width: 0%;
+    position: absolute;
+    bottom: -80%;
+    left: 60%;
+    @media (max-width: 1300px){
+        left: 0%;
     }
 `;
 
