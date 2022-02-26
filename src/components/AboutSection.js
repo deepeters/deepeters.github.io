0006dc71f9fd66ef21891a138c8cee3d94857b14
deepeters <dennis95peters@gmail.com>
2022-02-26 18:38:12 +0300

    Update about and questions sections information

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -4,20 +4,11 @@ import about from "../images/about.jpg";
 import { Link } from "react-router-dom";
 
 //Styled Components
-import {
-  StyledAbout,
-  StyledDescription,
-  StyledImage,
-  StyledHide,
-} from "../styles";
+import { StyledAbout, StyledDescription, StyledImage, StyledHide } from "../styles";
 
 //Framer Motion
 import { motion } from "framer-motion";
-import {
-  titleAnim,
-  fade,
-  photoAnim,
-} from "../animation";
+import { titleAnim, fade, photoAnim } from "../animation";
 import Wave from "./Wave";
 
 const AboutSection = () => {
@@ -26,9 +17,7 @@ const AboutSection = () => {
       <StyledDescription>
         <motion.div className="title">
           <StyledHide>
-            <motion.h2 variants={titleAnim}>
-              Hello, I am Dennis
-            </motion.h2>
+            <motion.h2 variants={titleAnim}>Hello, I am Dennis</motion.h2>
           </StyledHide>
           <StyledHide>
             <motion.h2 variants={titleAnim}>
@@ -36,62 +25,35 @@ const AboutSection = () => {
             </motion.h2>
           </StyledHide>
           <StyledHide>
-            <motion.h2 variants={titleAnim}>
-              an African literature enthuthiast
-            </motion.h2>
+            <motion.h2 variants={titleAnim}>an African literature enthuthiast</motion.h2>
           </StyledHide>
         </motion.div>
         <motion.p variants={fade}>
           <p>
-            Working in Nairobi City, Kenya, I have
-            around three to four years in software
-            development and contributed to more
-            than twenty open-source application
-            projects to the online community.
+            Working in Nairobi City, Kenya, I have around three to four years in software development and contributed to
+            more than thirty open-source application projects to the online community.
             <br />
             <br />
-            As a software engineer, I have worked
-            developed, tested, deployed, and
-            maintained solutions ranging from
-            banking, inventory, payroll, and human
-            resource information management
-            systems among others. I have developed
-            and secured RESTful APIs and Web
-            Services to share intricate resources
-            between application software. Applied
-            the microservice architecture using
-            Spring Boot & ASP.NET and enabled
-            communication, service discovery,
-            fault tolerance, resilience and
-            optimisation. Secured all the
-            components of software on both the
-            application and the network layer and
-            engaged in intensive testing on
-            different levels including unit,
-            functional and integration tests.
+            As a software engineer, I have worked developed, tested, deployed, and maintained solutions ranging from
+            banking, aviation, inventory, payroll, and human resource information management systems among others. I
+            have developed and secured RESTful APIs and Web Services to share intricate resources between application
+            software. Applied the microservice architecture using Spring Boot & ASP.NET and enabled communication,
+            service discovery, fault tolerance, resilience and optimisation. Secured all the components of software on
+            both the application and the network layer and engaged in intensive testing on different levels including
+            unit, functional and integration tests.
             <br />
             <br />
-            As an innovator and solution finder, I
-            have attempted to develop a solution
-            that combines my discreet passion for
-            creative arts, in particular, African
-            literature with technological tools
-            and techniques.
+            As an innovator and solution finder, I have attempted to develop a solution that combines my discreet
+            passion for creative arts, in particular, African literature with technological tools and techniques.
           </p>
         </motion.p>
         <motion.button variants={fade}>
           {" "}
-          <Link to="/projects">
-            View Projects
-          </Link>
+          <Link to="/projects">View Projects</Link>
         </motion.button>
       </StyledDescription>
       <StyledImage>
-        <motion.img
-          variants={photoAnim}
-          src={about}
-          alt="Dennis is a portrait"
-        />
+        <motion.img variants={photoAnim} src={about} alt="Dennis is a portrait" />
       </StyledImage>
       <Wave />
     </StyledAbout>
--- src/components/QuestionsSection.js
+++ src/components/QuestionsSection.js
@@ -1,118 +1,187 @@
-import React from 'react';
+import React from "react";
 
 //Styles
 import styled from "styled-components";
-import { StyledAbout } from '../styles';
+import { StyledAbout } from "../styles";
 
-import Toggle from './Toggle';
+import Toggle from "./Toggle";
 
-import { AnimateSharedLayout } from 'framer-motion';
+import { AnimateSharedLayout } from "framer-motion";
 //import { useScroll } from "./useScroll";
 
-
 const QuestionsSection = () => {
+  //const [element, controls] = useScroll();
 
-    //const [element, controls] = useScroll();
+  return (
+    <StyledQuestions
+    // variants = { scrollReveal }
+    // ref = { element }
+    // animate = { controls }
+    // initial = "hidden"
+    >
+      <h2>
+        {" "}
+        Any Questions? <span> FAQs </span>{" "}
+      </h2>
+      <AnimateSharedLayout>
+        <Toggle title="What is Dennis's TechStack and Skillset?">
+          <div className="answer">
+            <p>
+              <ul>
+                <li>Software immersive full stack development, implementation, testing and support in: </li>
+                <ul>
+                  <li>
+                    Java: Developing and Securing RESTful APIs and RESTful Web Services with JAX-RS, development of SOAP
+                    Web Services with JAX-WS, application of the Spring Framework for Enterprise Java applications.,
+                    bootstrapping Spring Applications with Spring Boot MVC, implementation of the Microservice
+                    architecture with Spring Boot including communication and service discovery, fault tolerance,
+                    resilience and configuration and unit testing with Junit 5.{" "}
+                  </li>
+                  <li>
+                    C#: Web development using the .NET (ASP.NET & ASP.NET Core) MVC framework including deconstruction
+                    of regular monolith web applications to micro services and migration to Episerver Content Management
+                    Systems (CMS) 11 & 12
+                  </li>
+                  <li>JavaScript: Client side application development with React JS and Angular JS.</li>
+                  <li>Python: End to end application development with Flask and Django.</li>
+                </ul>
+                <li>
+                  {" "}
+                  Database Management; an understanding in structured query language (SQL) including PostgreSQL, Oracle
+                  and MySQL, knowledge of relational database management, object-oriented database management systems
+                  and NoSQL frameworks like Firebase.{" "}
+                </li>
+                <li>
+                  {" "}
+                  Information security comprising user authentication, cryptography, white coat ethical hacking, digital
+                  forensics, penetration testing, logs management, virtualisation, malware reverse engineering, scanning
+                  networks and vulnerability analysis{" "}
+                </li>
+                <li> Familiarity with the Agile Scrum Methodology in Software development and delivery. </li>
+                <li> Version Control Tools, i.e. Git </li>
+              </ul>
+            </p>
+          </div>
+        </Toggle>
+        <Toggle title="Which schools has Dennis attended?">
+          <div className="answer">
+            <h4>
+              {" "}
+              2020 - 2021 || <span> Moringa School </span> || Software Immersive FullStack Development{" "}
+            </h4>
+            <h4>
+              {" "}
+              2019 - 2019 || <span> Strathmore University </span> || Information Systems Security & Certified Ethical
+              Hacking (CEHv10){" "}
+            </h4>
+            <h4>
+              {" "}
+              2013 - 2018 || <span> Dedan Kimathi University of Technology </span> || Bachelor of Science (BSc.)
+              Information Technology{" "}
+            </h4>
+          </div>
+        </Toggle>
+        <Toggle title="Where has Dennis worked?">
+          <div className="answer">
+            <h4>
+              {" "}
+              2021 – to date || <span> Software Engineer </span> || Kenya Airways PLC{" "}
+            </h4>
+            <p>
+              <ul>
+                <li>Software development for web applications in C#, Java and JavaScript programming languages.</li>
+                <li>Deconstructing large .NET monolith applications to micro services.</li>
+                <li>
+                  Implementing hybrid cloud infrastructure on Azure Cloud in particular migration of complex
+                  infrastructure to Episerver CMS.
+                </li>
+                <li>Automation of deployment processes by building CI/CD pipelines.</li>
+                <li>
+                  Application, design and utilisation of third party APIs such the Safaricom Mpesa B2C, B2B and C2B
+                  payment API as well as other direct payment option integrations.
+                </li>
+              </ul>
+            </p>
 
-    return (
-        <StyledQuestions
-            // variants = { scrollReveal } 
-            // ref = { element } 
-            // animate = { controls } 
-            // initial = "hidden"
-        >
-            <h2> Any Questions? <span> FAQs </span> </h2>
-            <AnimateSharedLayout> 
-                <Toggle title = "What is Dennis's TechStack and Skillset?">              
-                        <div className="answer">
-                            <p> 
-                                <ul>
-                                    <li> Software immersive full stack development, implementation, testing and support in JavaScript,CSS,HTML, Java and Python with application skills in version control tools like Git, open-source software web application frameworks such as Spark, SpringBoot, Django, Flask, React JS and Angular JS and a collection of useful frontend libraries. </li>
-                                    <li> Android Programming with Java & Kotlin programming languages. </li>
-                                    <li> Database Management; an understanding in structured query language (SQL) including PostgreSQL and MySQL, knowledge of relational database management, object-oriented database management systems and NoSQL frameworks like Firebase. </li>
-                                    <li> Implementation of software architectural styles for designing networked applications such as Representational State Transfer (REST) APIs. </li>
-                                    <li> Information security comprising white coat ethical hacking, digital forensics, penetration testing, logs management, virtualisation, malware reverse engineering, scanning networks, vulnerability analysis, cloud computing and cryptography. </li>
-                                    <li> Systems Administration with responsibilities to set up and monitor the operations and maintenance of computer systems. </li>
-                                    <li> IT Support including product version updates, critical infrastructure updates, vendor clarification and support, compliance with IT regulations, outsourcing and troubleshooting computer systems and operations. </li>
-                                    <li> Customer Technical Support. </li>
-                                </ul>
-                            </p>
-                   
-                        </div>
-                </Toggle>
-                <Toggle title="Which schools has Dennis attended?">            
-                        <div className="answer">
-                            <h4> 2021 - 2021 || <span> HarvardX (Harvard on edX) </span> || CS50's Web Programming with Python and Javascript </h4>
-                            <h4> 2020 - 2021 || <span> Google Africa Developer Scholarship </span> || Android Developer Foundations & Kotlin App Fundamentals </h4>
-                            <h4> 2020 - 2021 || <span>  Moringa School </span> || Software Immersive FullStack Development </h4>
-                            <h4> 2019 - 2019 || <span>  Strathmore University </span> || Information Systems Security & Certified Ethical Hacking (CEHv10) </h4>
-                            <h4> 2013 - 2018 || <span> Dedan Kimathi University of Technology </span> || Bachelor of Science (BSc.) Information Technology </h4>
-                            <h4> 2012 - 2013 || <span> St. Joseph Computer College </span> || Diploma in Information Communication Technlogy </h4>
-                           
-                        </div>        
-                </Toggle>
-                <Toggle title = "Where has Dennis worked?"> 
-                        <div className="answer">
-                            <h4> 2021 – to date || <span>  Software Engineer </span> || Techsavanna Technology - Kenya Airways </h4>
-                            <p>
-                                <ul>
-                                    <li> Fullstack Web Development at Kenya Airways </li>
-                                </ul>
-                            </p>
+            <h4>
+              {" "}
+              2019 - 2020 || <span> Software Implementation & Support Engineer </span> || List Fintech Solutions Ltd.{" "}
+            </h4>
+            <p>
+              <ul>
+                <li>
+                  {" "}
+                  Managed Citius Core Banking, Agency Banking, Mobile Banking and Internet Banking system projects in
+                  different roles – implementation, testing, migration training etc.{" "}
+                </li>
+                <li> Oracle Database Management. </li>
+                <li>
+                  {" "}
+                  Applied Citius systems on both Cloud Systems and In-premise data centre systems for List global
+                  clients.{" "}
+                </li>
+                <li>
+                  {" "}
+                  User Acceptance Testing and training & Citius Digital Banking Solutions development, maintenance and
+                  support.{" "}
+                </li>
+              </ul>
+            </p>
 
-                            <h4> 2019 - 2020 || <span>  Software Implementation & Support Engineer </span> || List Fintech Solutions Ltd. </h4>
-                            <p>
-                                <ul>
-                                    <li> Managed Citius Core Banking, Agency Banking, Mobile Banking and Internet Banking system projects in different roles – implementation, testing, migration training etc. </li>
-                                    <li> Oracle Database Management. </li>
-                                    <li> Applied Citius systems on both Cloud Systems and In-premise data centre systems for List global clients. </li>
-                                    <li> User Acceptance Testing and training & Citius Digital Banking Solutions development, maintenance and support. </li>
-                                </ul>
-                            </p>
+            <h4>
+              {" "}
+              2018 || <span> Software Engineer </span> || Realtime Technologies Ltd.{" "}
+            </h4>
+            <p>
+              <ul>
+                <li> Sybase Database Management. </li>
+                <li> PowerBuilder Application Development. </li>
+                <li> Customer Service. </li>
+              </ul>
+            </p>
 
-                            <h4> 2018 || <span>  Software Engineer </span> || Realtime Technologies Ltd. </h4>
-                            <p>
-                                <ul>
-                                    <li> Sybase Database Management. </li>
-                                    <li> PowerBuilder Application Development. </li>
-                                    <li> Customer Service. </li>
-                                </ul>
-                            </p>
+            <h4>
+              {" "}
+              2016 || <span> IT Support </span> || Kenya Power Company,{" "}
+            </h4>
+            <p>
+              <ul>
+                <li> Assisted in contracting of customers. </li>
+                <li> Data gathering and processing. </li>
+              </ul>
+            </p>
 
-                            <h4> 2016 ||  <span>  IT Support </span> || Kenya Power Company, </h4>
-                            <p>
-                                <ul>
-                                    <li> Assisted in contracting of customers. </li>
-                                    <li> Data gathering and processing. </li>
-                                </ul>
-                            </p>
+            <h4>
+              {" "}
+              2016 || <span> Management Intern </span> || National Hospital Insurance Fund,{" "}
+            </h4>
+            <p>
+              <ul>
+                <li> Capturing of claims, confirming of claims, claims examination and filling. </li>
+                <li>
+                  {" "}
+                  Collaborated with organisation technician that, the computer cluster is functioning efficiently.{" "}
+                </li>
+                <li> Scanning, merging, and printing cards and photo card dispatch. </li>
+                <li> Customer care procedures registration of new members. </li>
+                <li> Accounting and audit procedures. </li>
+              </ul>
+            </p>
+          </div>
+        </Toggle>
 
-                            <h4> 2016 || <span>  Management Intern </span> || National Hospital Insurance Fund, </h4>
-                            <p>
-                                <ul>
-                                    <li> Capturing of claims, confirming of claims, claims examination and filling. </li>
-                                    <li> Collaborated with organisation technician that, the computer cluster is functioning efficiently. </li>
-                                    <li> Scanning, merging, and printing cards and photo card dispatch. </li>
-                                    <li> Customer care procedures registration of new members. </li>
-                                    <li> Accounting and audit procedures. </li>
-                                </ul>
-                            </p>
-                        </div>
-                </Toggle>
-                
-                <Toggle title = "Where does Dennis want to work?">               
-                        <div className="answer">
-                            <p>Lorem ipsum dolor sit amet.</p>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
-                        </div>
-                    
-                </Toggle>
-            </AnimateSharedLayout>
-        </StyledQuestions>
-    )
-}
+        <Toggle title="Where does Dennis want to work?">
+          <div className="answer">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, corrupti?</p>
+          </div>
+        </Toggle>
+      </AnimateSharedLayout>
+    </StyledQuestions>
+  );
+};
 
-const StyledQuestions = styled (StyledAbout)`
+const StyledQuestions = styled(StyledAbout)`
     display: block;
     span {
         display: block;
@@ -154,6 +223,5 @@ const StyledQuestions = styled (StyledAbout)`
         }
     }
 `;
-
 
 export default QuestionsSection;
